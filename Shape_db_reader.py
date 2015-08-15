f9287f5e442f4c1641cd63dd33567ded2b9e6f3f
kamaleshwar <kp2601@rit.edu>
2015-08-15 15:10:11 -0400

    Delete Shape_db_reader.py

--- Shape_db_reader.py
+++
@@ -1,9 +0,0 @@
-# Read the field descriptors for the database file
-sf.fields
-[("DeletionFlag", "C", 1, 0), ["AREA", "N", 18, 5],
-["BKG_KEY", "C", 12, 0], ["POP1990", "N", 9, 0], ["POP90_SQMI", "N", 10, 1],
-["HOUSEHOLDS", "N", 9, 0],
-["MALES", "N", 9, 0], ["FEMALES", "N", 9, 0]]
-# Read the 2nd and 3rd field values of the 4th database record
-sf.records[3][1:3]
-['060750601001', 4715]
