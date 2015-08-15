ccd006105eb83b6ff646e974afed7b001eb4ee47
kamaleshwar <kp2601@rit.edu>
2015-08-15 15:11:08 -0400

    Create Csv2Excel.py

---
+++ Csv2Excel.py
@@ -0,0 +1,33 @@
+import os
+import glob
+import csv
+import shutil
+from xlsxwriter.workbook import Workbook
+
+source_dir= 'Discretized_data_set'
+target_dir= 'Final_data_set'
+
+print "Converting files to required format..."
+
+if (os.path.isdir("Final_data_set")):
+    shutil.rmtree('Final_data_set')
+
+files = os.listdir(source_dir)
+
+for file in files:
+    if(not os.path.isdir(target_dir)):
+        os.mkdir(target_dir)
+    workbook = Workbook( target_dir+"\\"+file.split(".")[0] + '.xlsx')
+    worksheet = workbook.add_worksheet()
+    with open(source_dir+"\\"+file, 'rb') as f:
+        reader = csv.reader(f)
+        for r, row in enumerate(reader):
+            for c, col in enumerate(row):
+                worksheet.write(r, c, col)
+    workbook.close()
+    f.close()
+
+shutil.rmtree(source_dir)
+
+print "Comversion Completed"
+
